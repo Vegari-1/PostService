b1e3ab276d07bf973c4f569da5dc172446f1f60a
Nikola <nikolakabasaj6@gmail.com>
2022-08-25 11:26:38 +0200

    feat: ci pipe for pushing .Net application added

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	branches: ["main", {"name": "develop", "prerelease": true}],
+	repositoryUrl: "https://github.com/Vegari-1/PostService",
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/github",
+		"semantic-release-export-data"
+	]
+}
